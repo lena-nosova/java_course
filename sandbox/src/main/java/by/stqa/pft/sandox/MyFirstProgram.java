0681b93c952444572110035b7ceea27039ce9130
LN <l.nosova16@yandex.ru>
2021-10-10 22:53:34 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/by/stqa/pft/sandox/MyFirstProgram.java
+++ sandbox/src/main/java/by/stqa/pft/sandox/MyFirstProgram.java
@@ -3,8 +3,28 @@ package by.stqa.pft.sandox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("MY NEW PROJECT");
+        project("project");
+        project("project House");
+        project("project Home");
 
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь со сторонами " + a +  " и " + b + " = " + area(a, b));
+
+        double l = 5;
+            System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    }
+    public static void project(String somebody) {
+           System.out.println("My new " + somebody + "!");
+
+    }
+    public static double area(double len){
+        return len*len;
+    }
+
+    public static double area(double a, double b){
+        return a * b;
     }
 
  }
